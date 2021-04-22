4d852bc849a6616bad3388a15f6962765bbd25db
Gonzo <gvidal.industrial@gmail.com>
2021-04-22 16:26:24 -0500

    Adding es7 features

---
+++ src/es7/index.js
@@ -0,0 +1,10 @@
+//Includes method
+let numbers = [1,2,5,6,77,200];
+console.log(numbers.includes(6) ? "Si se encuentra el valor" : "No se encuentra el valor");
+
+//Exponential
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
