fd14e745944e3724ac8cf283c7ff188ff2bc650b
Gonzo <gvidal.industrial@gmail.com>
2021-04-22 18:21:15 -0500

    Adding some es9 features

---
+++ src/es9/index.js
@@ -0,0 +1,22 @@
+//ES9
+const obj =  {
+  name: "Gonzalo",
+  age: 32,
+  country: "Perú",
+};
+//Destructuring assignment
+let { name } = obj;
+console.log(name);
+
+//Destructuring + spread operator
+const obj =  {
+  name: "Gonzalo",
+  age: 32,
+  country: "Perú",
+};
+//Destructuring assignment
+let { name, ...all } = obj;
+console.log(name, all);
+//rest: data I want
+let { country, ...rest } = obj;
+console.log(rest);
